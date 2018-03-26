84236170cdf2a1d18dd57ee490f4b4e3bb22e6b9
Jason <jporter7@ycp.edu>
2018-03-26 15:50:16 -0400

    Second commit

---
+++ src/main/java/com/example/product/web/app/ProductWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.product.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductWebAppApplication.class, args);
+	}
+}
