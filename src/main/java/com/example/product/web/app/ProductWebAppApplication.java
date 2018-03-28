3439c1005d0acba8ba6c2bbaa80c91417b11a4d3
Jason <jporter7@ycp.edu>
2018-03-27 21:55:16 -0400

    Fixing autowiring issue and creating VendorProductListController and ProductListController

--- src/main/java/com/example/product/web/app/ProductWebAppApplication.java
+++ src/main/java/com/example/product/web/app/ProductWebAppApplication.java
@@ -2,8 +2,10 @@ package com.example.product.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.kleancierge.product.api")
 public class ProductWebAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/product/web/app/controllers/vendor/VendorListController.java
+++ src/main/java/com/example/product/web/app/controllers/vendor/VendorListController.java
@@ -1,27 +1,26 @@
-package com.example.product.web.app.controllers.product;
+package com.example.product.web.app.controllers.vendor;
 
 import com.kleancierge.product.api.contract.FieldErrors;
 import com.kleancierge.product.api.contract.Result;
-import com.kleancierge.product.api.contract.product.IVendorProductListService;
-import com.kleancierge.product.api.contract.product.VendorProductListService;
-import com.kleancierge.product.api.model.product.Model;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kleancierge.product.api.contract.vendor.IVendorListService;
+import com.kleancierge.product.api.contract.vendor.VendorListService;
+import com.kleancierge.product.api.model.vendor.Model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class VendorProductListController {
-    private IVendorProductListService listService;
+public class VendorListController {
+    private IVendorListService listService;
 
     private Result result;
 
-    public VendorProductListController(IVendorProductListService listService) { this.listService = listService; }
+    public VendorListController(IVendorListService listService) { this.listService = listService; }
 
     @GetMapping(value = "/products/{vendorId}")
-    public Result list(Long vendorId, Pageable page) {
-        listService.execute(vendorId, page, new VendorProductListService.ServiceResponse() {
+    public Result list(Pageable page) {
+        listService.execute(page, new VendorListService.ServiceResponse() {
             @Override
             public void errors(FieldErrors fieldErrors) { result = Result.ERROR(fieldErrors); }
 
