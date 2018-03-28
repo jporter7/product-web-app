1172930e39f6706519c2e7b78a50688c764fad45
Jason <jporter7@ycp.edu>
2018-03-28 12:39:46 -0400

    Fixing componentscan issues. The web app controllers/pages now connect and can be worked on

--- src/main/java/com/example/product/web/app/controllers/ProductWebAppApplication.java
+++ src/main/java/com/example/product/web/app/controllers/ProductWebAppApplication.java
@@ -1,11 +1,19 @@
-package com.example.product.web.app;
+package com.example.product.web.app.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//this component scan is needed to autowire and
+//load everyything from the server-side
+//kleancierge app
 @ComponentScan("com.kleancierge.product.api")
+//this component scan is needed because the above
+//component scan apparently makes the springbootapp
+//annotation not load the controllers and other
+//items in this web-app
+@ComponentScan
 public class ProductWebAppApplication {
 
 	public static void main(String[] args) {
