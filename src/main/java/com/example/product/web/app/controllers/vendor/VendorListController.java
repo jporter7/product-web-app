0975f32f84e3536b1fa0474947661b7c6fab7eca
Jason <jporter7@ycp.edu>
2018-03-27 22:56:21 -0400

    Attempting some progress

--- src/main/java/com/example/product/web/app/controllers/vendor/VendorListController.java
+++ src/main/java/com/example/product/web/app/controllers/vendor/VendorListController.java
@@ -7,18 +7,21 @@ import com.kleancierge.product.api.contract.vendor.VendorListService;
 import com.kleancierge.product.api.model.vendor.Model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+
+@Controller
 public class VendorListController {
     private IVendorListService listService;
 
     private Result result;
 
-    public VendorListController(IVendorListService listService) { this.listService = listService; }
+    public VendorListController() {}//IVendorListService listService) { this.listService = listService; }
 
-    @GetMapping(value = "/vendor")
+    /*@GetMapping(value = "/vendor")
     public Result list(Pageable page) {
         listService.execute(page, new VendorListService.ServiceResponse() {
             @Override
@@ -29,5 +32,15 @@ public class VendorListController {
         });
 
         return result;
+    }*/
+
+    @GetMapping(value = "/vendor", produces = "text/html")
+    public String list(){
+        return "redirect:/vendorlistpage.html";
     }
+
+    /*@RequestMapping(value = "/vendor", method = RequestMethod.GET)
+    public String index() {
+        return "vendorlistpage";
+    }*/
 }
