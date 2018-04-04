6a7f1f99f4d299511ab55b024ad72d85632c9440
Jason <jporter7@ycp.edu>
2018-04-04 11:42:25 -0400

    Creating product update status rest controller

---
+++ src/main/java/com/example/product/web/app/product/ProductUpdateStatusRestController.java
@@ -0,0 +1,32 @@
+package com.example.product.web.app.product;
+
+import com.kleancierge.product.api.contract.FieldErrors;
+import com.kleancierge.product.api.contract.Result;
+import com.kleancierge.product.api.contract.product.IProductUpdateStatusService;
+import com.kleancierge.product.api.contract.product.ProductUpdateStatusService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductUpdateStatusRestController {
+    private IProductUpdateStatusService updateStatusService;
+
+    private Result result;
+
+    public ProductUpdateStatusRestController(IProductUpdateStatusService updateStatusService) {
+        this.updateStatusService = updateStatusService;
+    }
+
+    @PutMapping(value = "/product/update-status/{productId}")
+    public Result updateStatus(Long id, String status) {
+        updateStatusService.execute(id, status, new ProductUpdateStatusService.ServiceResponse() {
+            @Override
+            public void errors(FieldErrors fieldErrors) { result = Result.ERROR(fieldErrors); }
+
+            @Override
+            public void success(Long id) { result = Result.SUCCESS(id); }
+        });
+
+        return result;
+    }
+}
