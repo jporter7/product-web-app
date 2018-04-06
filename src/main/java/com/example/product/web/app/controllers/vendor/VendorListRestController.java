7d401ec7ec4573281f269f507d7141c6fbaeb186
Jason <jporter7@ycp.edu>
2018-04-06 15:38:58 -0400

    Adding page information as parameters in vendorlistrestcontroller

--- src/main/java/com/example/product/web/app/controllers/vendor/VendorListRestController.java
+++ src/main/java/com/example/product/web/app/controllers/vendor/VendorListRestController.java
@@ -7,8 +7,10 @@ import com.kleancierge.product.api.contract.vendor.IVendorListService;
 import com.kleancierge.product.api.contract.vendor.VendorListService;
 import com.kleancierge.product.api.model.vendor.Model;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +21,11 @@ public class VendorListRestController {
 
     public VendorListRestController(IVendorListService listService) { this.listService = listService; }
 
-    @GetMapping(value = "/vendors/list/{pageNumber}")
-    public Result list(Pageable page) {
-        listService.execute(page, new VendorListService.ServiceResponse() {
+    @GetMapping(value = "/vendors/list")
+    public Result list(@RequestParam("pageSize") int pageSize, @RequestParam("pageIndex") int pageIndex) {
+        PageRequest request = new PageRequest(pageIndex, pageSize);
+
+        listService.execute(request, new VendorListService.ServiceResponse() {
             @Override
             public void errors(FieldErrors fieldErrors) { result = Result.ERROR(fieldErrors); }
 
