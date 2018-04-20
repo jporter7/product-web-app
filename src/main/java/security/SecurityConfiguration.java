6bbcca57c627cf9172da5dfea3b0bda3a242a4c8
Derek McClellan <dmcclellan@ycp.edu>
2018-04-20 14:41:53 -0400

    Implementation of authorization in config for vendor/product/phone sans retrieve/update

--- src/main/java/security/SecurityConfiguration.java
+++ src/main/java/security/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,7 +24,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/product/{vendorId}").permitAll()
-                .antMatchers("/vendors/page").permitAll();
+                    .antMatchers("/vendors/{vendorId}/products").hasRole("VENDOR")
+                    .antMatchers("/products/{id}/status").hasRole("VENDOR")
+                    .antMatchers("/vendors/{id}/status").hasRole("ADMIN")
+                    .antMatchers("/vendors/{id}/products").permitAll()
+                    .antMatchers("/vendors").permitAll()
+                    .antMatchers("/contacts/{id}/phones").permitAll()
+                    .antMatchers("/contacts/{contactId}/phones").hasRole("VENDOR")
+                    .antMatchers("/contacts/{contactId}/phones").hasRole("CLEANER");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication()
+                    .withUser("kcadmin").password("vinceisgreat").roles("ADMIN");
     }
 }
